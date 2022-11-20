e87ef3ba9489ca4f772c7365e26bf28041fc2c96
spykid-111 <rrsingh1101@gmail.com>
2022-11-20 17:37:29 +0530

    admin-form done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Resources from './pages/resources'
+import React from "react";
+import ResourceData from './pages/adminData';
+import Home from './pages/homepage';
 
 function App() {
 
   return (
     <div className="App">
-      <Resources />
+      <ResourceData />
     </div>
   )
 }
