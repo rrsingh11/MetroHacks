1978ebe4ca1852b77ab1129cb11810a13e197ca8
sudoer777 <78781902+sudoer777@users.noreply.github.com>
2022-11-20 01:05:40 -0600

    Add resourceAvailabilities API

--- routes/hospital.js
+++ routes/hospital.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import hospitals from '../data/hospital.js';
+import availabilities from '../data/resources/availability.js';
 var router = express.Router();
 
 /* POST hospital */
@@ -25,6 +26,26 @@ router.delete('/id/:hospitalId', async function(req, res) {
 /* PUT hospital */
 router.put('/id/:hospitalId', async function(req, res) {
     res.json(await hospitals.rename(req.params.hospitalId, req.body.name));
+});
+
+/* GET resource availability */
+router.get('/id/:hospitalId/resource/id/:resourceId', async function(req, res) {
+    res.json(await availabilities.getByHospitalAndResource(req.params.hospitalId, req.params.resourceId));
+});
+
+/* POST resource availability */
+router.post('/id/:hospitalId/resource/id/:resourceId', async function(req, res) {
+    res.json(await availabilities.create(req.params.hospitalId, req.params.resourceId));
+});
+
+/* DELETE resource availability */
+router.post('/id/:hospitalId/resource/id/:resourceId', async function(req, res) {
+    res.json(await availabilities.removeByHospitalAndResource(req.params.hospitalId, req.params.resourceId));
+});
+
+/* PUT resource availability */
+router.put('/id/:hospitalId/resource/id/:resourceId', async function(req, res) {
+    res.json(await availabilities.update(req.params.hospitalId, req.params.resourceId, req.body.available, req.body.patients));
 });
 
 //module.exports = router;
