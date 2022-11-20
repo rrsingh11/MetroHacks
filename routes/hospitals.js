4698bff10efed2056a97924ee4121ce35b36ed41
sudoer777 <78781902+sudoer777@users.noreply.github.com>
2022-11-19 23:43:31 -0600

    Add hospitals API

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import hospitals from '../data/hospitals.js';
 var router = express.Router();
 
+/* POST hospital */
+router.post('/', async function(req, res) {
+    res.json(await hospitals.create(req.body.name));
+});
+
 /* GET hospitals */
-router.get('/', function(req, res) {
-    res.json({test: "test"});
+router.get('/', async function(req, res) {
+   res.json(await hospitals.getAll());
+});
+
+/* GET hospital by id */
+router.get('/id/:hospitalId', async function(req, res) {
+    res.json(await hospitals.getById(req.params.hospitalId));
+});
+
+/* DELETE hospital by id */
+router.delete('/id/:hospitalId', async function(req, res) {
+    res.json(await hospitals.remove(req.params.hospitalId));
+});
+
+/* PUT hospital */
+router.put('/id/:hospitalId', async function(req, res) {
+    res.json(await hospitals.rename(req.params.hospitalId, req.body.name));
 });
 
 //module.exports = router;
