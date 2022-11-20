755a004e2b734af47e1370752b2be1df98b26fb8
spykid-111 <rrsingh1101@gmail.com>
2022-11-20 19:05:34 +0530

    Profile Data page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import Resources from './pages/resources'
+import reactLogo from './assets/react.svg'
 
+import Resources from './pages/resources'
+import Profile from './pages/profile'
 function App() {
 
   return (
     <div className="App">
-      <Resources />
+      <Profile />
     </div>
   )
 }
